c8602165bb6ee996992db8cc9b7204d0d474092f
João Victor Gontijo <jvgontijo166@gmail.com>
2024-08-03 14:47:04 -0300

    First commit

---
+++ src/main/java/br/com/infnet/appJoaoVictorGontijo/AppJoaoVictorGontijoApplication.java
@@ -0,0 +1,13 @@
+package br.com.infnet.appJoaoVictorGontijo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppJoaoVictorGontijoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppJoaoVictorGontijoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/infnet/appJoaoVictorGontijo/AppJoaoVictorGontijoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.infnet.appJoaoVictorGontijo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppJoaoVictorGontijoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
