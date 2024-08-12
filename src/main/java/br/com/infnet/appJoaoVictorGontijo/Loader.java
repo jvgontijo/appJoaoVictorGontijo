5dde97da724b7131d4413fcdb2bd09939bae9ced
João Victor Gontijo <jvgontijo166@gmail.com>
2024-08-11 23:32:02 -0300

    feature: Alterado o funcionamento dos Loaders, adaptando para os Services

--- src/main/java/br/com/infnet/appJoaoVictorGontijo/Loader.java
+++ src/main/java/br/com/infnet/appJoaoVictorGontijo/Loader.java
@@ -1,15 +1,30 @@
 package br.com.infnet.appJoaoVictorGontijo;
 
 import br.com.infnet.appJoaoVictorGontijo.character.CharacterLoader;
+import br.com.infnet.appJoaoVictorGontijo.character.CharacterService;
 import br.com.infnet.appJoaoVictorGontijo.user.UserLoader;
+import br.com.infnet.appJoaoVictorGontijo.user.UserService;
+
+import java.io.IOException;
 
 public class Loader {
 
-    public static void main(String[] args) {
-        UserLoader userLoader = new UserLoader();
+    public static void main(String[] args) throws IOException {
+        UserService userService = new UserService();
+        CharacterService characterService = new CharacterService();
+
+        UserLoader userLoader = new UserLoader(userService, characterService);
+        CharacterLoader characterLoader = new CharacterLoader(characterService);
+
+        characterLoader.loadFromFile();
         userLoader.loadFromFile();
 
-        CharacterLoader characterLoader = new CharacterLoader();
-        characterLoader.loadFromFile();
+        System.out.println(" --- Inicio UserLoader ---");
+        System.out.println(userLoader);
+        System.out.println(" --- Fim UserLoader ---");
+
+        System.out.println(" --- Inicio Character Loading ---");
+        System.out.println(characterLoader);
+        System.out.println(" --- Fim Character Loading ---");
     }
 }
