f6eb397eaddbfb79e45794f3529bcdba48badc63
João Victor Gontijo <jvgontijo166@gmail.com>
2024-08-11 21:00:46 -0300

    feature: Adicionado classes Person, User, Character, Appearance e os enums Weapon e Race

---
+++ src/main/java/br/com/infnet/appJoaoVictorGontijo/character/Race.java
@@ -0,0 +1,8 @@
+package br.com.infnet.appJoaoVictorGontijo.character;
+
+public enum Race {
+    HUMAN,
+    ELF,
+    GNOME,
+    DWARF
+}
---
+++ src/main/java/br/com/infnet/appJoaoVictorGontijo/character/Weapon.java
@@ -0,0 +1,13 @@
+package br.com.infnet.appJoaoVictorGontijo.character;
+
+public enum Weapon {
+    DAGGER,
+    STAFF,
+    SWORD,
+    BATTLEAXE,
+    WARHAMMER,
+    PISTOL,
+    BOOK,
+    BOW
+
+}
